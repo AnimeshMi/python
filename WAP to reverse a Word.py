8fdefd9722e2f38fdf0de6c48e43cc0ebd908a47
AnimeshMi <96002418+AnimeshMi@users.noreply.github.com>
2021-12-12 15:36:46 +0530

    Add files via upload

---
+++ WAP to reverse a Word.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program that accepts a word from the user and reverse it.
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+char = input("Enter a word  ")
+for reverse in range(len(char) -1,-1,-1):
+    print(char[reverse],end="")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
